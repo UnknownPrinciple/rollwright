caf9e91f1277b06cc7bc8366b3b784cc60dabe27
Oleksii Raspopov <oleksii.raspopov@gmail.com>
2024-03-25 15:41:23 -0400

    missing base path

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+	base: process.env.CI != null ? "/rollwright/" : undefined,
 	title: "Rollwright",
 	description:
 		"A set of tools to make Playwright comfortable for UI integration testing.",
